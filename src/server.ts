9db7aefd64edf38048b2c29ab57d7145edf565ba
unknown <emma.broudic35@gmail.com>
2023-12-15 14:34:48 +0100

    ajout des regets

--- src/server.ts
+++ src/server.ts
@@ -25,9 +25,6 @@ app.set('view engine', 'pug');
 // Définir le dossier des fichiers statiques (CSS, JavaScript, images, etc.)
 app.use(express.static(path.resolve(process.cwd(), 'src', 'styles')));
 
-// Définir le routeur pour la page d'accueil
-//app.use('/', indexRouter);
-
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Bienvenue sur mon serveur TypeScript !');
@@ -46,46 +43,53 @@ app.get('/form', (req: Request, res: Response) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/form', async (req, res) => {
-  
   const client = new Client(dbConfig);
 
   try {
-    // Connexion au client PostgreSQL
     await client.connect();
 
-    // Données du formulaire
-    //const messageTitle = req.body.Message_Title;
     const userFirstname = req.body.User_Firstname;
     const userSurname = req.body.User_Surname;
     const userEmail = req.body.User_Email;
     const messageTitle = req.body.Message_Title;
     const messageText = req.body.Message_Text;
 
-    console.log("test 1 : " + messageText);
+    // Expressions régulières pour les validations
+    const nameRegex = /^[a-zA-Z\-]{1,50}$/; // Contient moins de 50 caractères et autorise uniquement les lettres de l'alphabet et le tiret (-)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Valide l'adresse email
+    const titleRegex = /^.{1,100}$/; // Contient moins de 100 caractères
+    const textRegex = /^.{1,400}$/; // Contient moins de 400 caractères
 
-    // Requête SQL pour insérer les données dans la table
+    // Vérification des validations avec les expressions régulières
+    if (!nameRegex.test(userFirstname)) {
+      return res.status(400).send('Le prénom est invalide');
+    }
+    if (!nameRegex.test(userSurname)) {
+      return res.status(400).send('Le nom de famille est invalide');
+    }
+    if (!emailRegex.test(userEmail)) {
+      return res.status(400).send('L\'adresse email est invalide');
+    }
+    if (!titleRegex.test(messageTitle)) {
+      return res.status(400).send('Le titre du message est invalide');
+    }
+    if (!textRegex.test(messageText)) {
+      return res.status(400).send('Le texte du message est invalide');
+    }
+
     const insertQuery = `INSERT INTO message (User_Firstname, User_Surname, User_Email, Message_Title, Message_Text)
                          VALUES ($1, $2, $3, $4, $5)`;
-
-    console.log("Test 2 : " + insertQuery);
-    // Paramètres à passer à la requête
     const values = [userFirstname, userSurname, userEmail, messageTitle, messageText];
 
-    console.log("Test 3 : " + values);
-    // Exécution de la requête d'insertion
     await client.query(insertQuery, values);
-
     console.log('Nouvel enregistrement créé avec succès');
     res.send('Nouvel enregistrement créé avec succès');
   
   } catch (error) {
-    
     console.error('Erreur lors de l\'insertion des données :', error);
-    
     res.status(500).send(`Erreur lors de l'insertion des données : ${error}`);
   
   } finally {
-    // Fermeture de la connexion à la base de données
     await client.end();
   }
 });
